734c6c73d870c830f6fe1ad96ce692860a0eed67
bader <bader5uk@hotmail.com>
2024-10-07 13:25:14 +0400

    Takes array and treats it like a stack

--- StackUsingArray.java
+++ StackUsingArray.java
@@ -1,4 +1,6 @@
-import java.util.Stack;
+
+import java.util.ArrayList;
+
 
 public class StackUsingArray {
 
@@ -10,15 +12,27 @@ public class StackUsingArray {
              - Example Input: push(10); push(20); push(30); pop(); peek();
                Example Output: 30 // (result of pop) 20 // (result of peek)   */
 
-
+   static ArrayList<Integer> numbers = new ArrayList<>();
     public static void main(String[] args) {
-        Stack<Integer> numbers = new Stack<>();
-        numbers.push(10);
-        numbers.push(20);
-        numbers.push(30);
+        push(10);
+        push(20);
+        push(30);
         System.out.println("After push : " + numbers);
-        System.out.println("The first element popped: " + numbers.pop());
-        System.out.println("The element on top using th peak function: " + numbers.peek());
+        //pop();
+        peek();
 
+    }
+    public static void push(Integer n){
+        numbers.addFirst(n);
+    }
+    public static void pop(){
+        Integer x = numbers.getFirst();
+        numbers.remove(0);
+        System.out.println("Popped Element: " + x);
+    }
+
+    public static void peek(){
+        Integer x = numbers.getFirst();
+        System.out.println("The element on top is : "+ x);
     }
 }
